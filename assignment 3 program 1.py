44357bf1832f93297094a03c2042032d4fef17f6
Lakshmid24 <100596113+Lakshmid24@users.noreply.github.com>
2022-04-06 19:44:05 +0530

    Add files via upload

---
+++ assignment 3 program 1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Write a Python program to create a lambda function that adds 25 to a given number passed in as an argument.
+
+# In[2]:
+
+
+x=lambda a:a+25
+print(x(10))
+
+
+# In[ ]:
+
+
+
+
